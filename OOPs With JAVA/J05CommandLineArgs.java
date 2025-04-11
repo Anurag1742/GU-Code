2133dafbd7adc92c824626e5cd1c5da1fdc85c6d
Anurag1742 <anuragranjan373@gmail.com>
2025-04-12 00:33:55 +0530

    Create J05CommandLineArgs.java

---
+++ OOPs With JAVA/J05CommandLineArgs.java
@@ -0,0 +1,14 @@
+public class J05CommandLineArgs {
+    public static void main(String[] args) {
+        // Display the number of command-line arguments
+        System.out.println("Number of command-line arguments: " + args.length);
+
+        // Display each argument
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + (i+1) + ": " + args[i]);
+            
+        }
+        System.out.println("Anurag Ranjan");
+        System.out.println("24SCSE2030436");
+    }
+}
