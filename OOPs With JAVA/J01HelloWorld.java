522e859a99283544b27728f14f518270e529fc04
Anurag1742 <anuragranjan373@gmail.com>
2025-04-12 00:33:27 +0530

    Create J01HelloWorld.java

---
+++ OOPs With JAVA/J01HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class J01HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Anurag Ranjan");
+        System.out.println("24SCSE2030436");
+    }
+}
