63e169a55a898693818e5fa576a6c6c82d6ca4b4
Anurag1742 <anuragranjan373@gmail.com>
2025-04-12 00:33:50 +0530

    Create J04FactorialRecursion.java

---
+++ OOPs With JAVA/J04FactorialRecursion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class J04FactorialRecursion {
+
+    public static int Factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * Factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        sc.close();
+        System.out.println("Factorial of  " + num + " is  " + Factorial(num));
+        System.out.println("Anurag Ranjan");
+        System.out.println("24SCSE2030436");
+    }
+}
