e0f6295e25778d051c8d7c6ecd786f9cab817fdb
Anurag1742 <anuragranjan373@gmail.com>
2025-04-15 23:42:04 +0530

    Create P01Basics.py

---
+++ ML With Python/Python/P01Basics.py
@@ -0,0 +1,39 @@
+# Display the element 
+integer_var = 10  #integer
+float_var = 20.5  # float
+string_var = "Hello" #String
+boolean_var = True      #Boolean
+list_var = [1,2,3,4]    #List
+tuple_var = (5,6,7)     #Tuple
+dict_var = {"key": "value"} # Dictionary
+
+print("Integer:", integer_var, "Type:",type(integer_var))
+print("Float:", float_var, "Type:",type(float_var))
+print("String:", string_var, "Type:",type(string_var))
+print("Boolean:", boolean_var, "Type:",type(boolean_var))
+print("List:", list_var, "Type:",type(list_var))
+print("Tuple:", tuple_var, "Type:",type(tuple_var))
+print("Dictionary:", dict_var, "Type:",type(dict_var))
+
+
+x = 10
+y = 5
+
+# print (x+y)
+
+addition = x+y          #Addition
+subtraction = x-y       #Subtraction
+multiplication = x*y    #Multiplication
+division = x/y          #Division
+integer_division = x//y #Integer Division
+modulus = x%y           #Modulus
+exponentiation = x**y   #Exponentiation
+
+# Display results of arithmetic operations
+print("Addition : ", addition)
+print("Subtraction : ", subtraction)
+print("Multiplication : ", multiplication)
+print("Division : ", division)
+print("Integer Division : ", integer_division)
+print("Modulus : ", modulus)
+print("Exponentiation : ", exponentiation)
