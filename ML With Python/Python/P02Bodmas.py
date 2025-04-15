530a4f33f2aa1bd909f57479750c1e233eaa27e7
Anurag1742 <anuragranjan373@gmail.com>
2025-04-15 23:42:12 +0530

    Create P02Bodmas.py

---
+++ ML With Python/Python/P02Bodmas.py
@@ -0,0 +1,23 @@
+#Python 
+#CopyEdit
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a-b
+    
+def multiply(a,b):
+    return a*b
+    
+def divide(a,b):
+    if b!=0:
+        return a/b
+    else:
+        return"Error! Division by zero."
+#Testing the functions
+a=10
+b=5
+print("Addition: ",add(a,b))
+print("Subtraction: ",subtract(a,b))
+print("Multiplication: ",multiply(a,b))
+print("Division: ",divide(a,b))
